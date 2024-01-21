3bc37c8b514757e382c2cc82e1aa2993958b029f
TacianaFer <taci.frnnds@gmail.com>
2024-01-21 00:14:35 -0300

    Desenvolvendo Deleta registros

--- source/deleta_registro.py
+++ source/deleta_registro.py
@@ -1,2 +1,17 @@
-def deleta_registro():
-    pass
+# Função para deletar registros
+def deleta_registros():
+  # Obter o ID do registro a ser deletado
+  linhas[id] = f"{data},{registro},{valor}\n"
+  id = int(input("Digite o registro a ser deletado: "))
+
+  # Deletar o registro do arquivo
+  with open("registros.csv", "r") as arquivo:
+    # Ler as linhas do arquivo
+    linhas = arquivo.readlines()
+
+  # Deletar o registro
+  del linhas[id]
+
+  # Salvar o arquivo
+  with open("registros.csv", "w") as arquivo:
+    arquivo.writelines(linhas)
