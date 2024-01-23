c1295deefc8faf9c01a17d40c06844b423b9a55a
TacianaFer <taci.frnnds@gmail.com>
2024-01-22 22:22:51 -0300

    Função para criar um novo registro

--- source/atualiza_registro.py
+++ source/atualiza_registro.py
@@ -8,9 +8,21 @@ def atualizar_registros():
   # Obter o novo registro
   tipo = input("Digite o novo tipo de registro (despesa, receita, investimento): ")
 
-  # Obter o novo valor da movimentação
+  # Obter o novo valor do registro
   valor = float(input("Digite o novo valor do registro: "))
 
-  # Se o tipo de movimentação for despesa, multiplicar o valor por -1
+  # Se o tipo de registro for despesa, multiplicar o valor por -1
   if tipo == "despesa":
     valor *= -1
+
+    # Atualizar o registro no arquivo
+  with open("registros.csv", "r") as arquivo:
+    # Ler as linhas do arquivo
+    linhas = arquivo.readlines()
+
+  # Atualizar o registro
+  linhas[id] = f"{data},{tipo},{valor}\n"
+
+  # Salvar o arquivo
+  with open("registros.csv", "w") as arquivo:
+    arquivo.writelines(linhas)
