efe4ea12a76864f0bc9ae8f92c7c8d94ad617122
TacianaFer <taci.frnnds@gmail.com>
2024-01-22 22:12:30 -0300

    Função atualizar

--- source/atualiza_registro.py
+++ source/atualiza_registro.py
@@ -1,2 +1,16 @@
-def atualiza_registro():
-    pass
+def atualizar_registros():
+  # Obter o ID do registro a ser atualizado
+  id = int(input("Digite o ID do registro a ser atualizado: "))
+
+  # Obter a nova data
+  data = datetime.datetime.now()
+
+  # Obter o novo registro
+  tipo = input("Digite o novo tipo de registro (despesa, receita, investimento): ")
+
+  # Obter o novo valor da movimentação
+  valor = float(input("Digite o novo valor do registro: "))
+
+  # Se o tipo de movimentação for despesa, multiplicar o valor por -1
+  if tipo == "despesa":
+    valor *= -1
