a7eed2bbbab71854dd7bf3d2cacae01e32c3c0b1
Guilherme Oliveira <silva.guilhermeoliveira@gmail.com>
2024-01-24 21:50:56 -0300

    Update

--- source/main.py
+++ source/main.py
@@ -16,56 +16,55 @@ def cabecalho():
           f'{"="*70} \n')
 
 def main():
-
     cabecalho()
 
 
-aux = {
-    '1': cria_registro(),
-    '2': le_registro(),
-    '3': atualiza_registro(),
-    '4': deleta_registro(),
-    '5': mostra_features(),
-    '6': exporta_relatorio(),
-    '7': encerrar_programa()
-}
+    aux = {
+        '1': cria_registro(),
+        '2': le_registro(),
+        '3': atualiza_registro(),
+        '4': deleta_registro(),
+        '5': mostra_features(),
+        '6': exporta_relatorio(),
+        '7': encerrar_programa()
+    }
 
-tentativas = 0
+    tentativas = 0
 
-while True:
-    operacao = input('''
-                         
-    Digite a opção que deseja realizar:
+    while True:
+        operacao = input('''
+                            
+        Digite a opção que deseja realizar:
+            
+        [1] CRIAR UM REGISTRO                             
+        [2] LER UM REGISTRO                               
+        [3] ATUALIZAR UM REGISTRO                         
+        [4] DELETAR UM REGISTRO                           
+        [5] INFORMAÇÕES SOBRE SEUS INVESTIMENTOS   
+        [6] EXPORTAR RELATÓRIO                              
+        [7] SAIR  
         
-    [1] CRIAR UM REGISTRO                             
-    [2] LER UM REGISTRO                               
-    [3] ATUALIZAR UM REGISTRO                         
-    [4] DELETAR UM REGISTRO                           
-    [5] INFORMAÇÕES SOBRE SEUS INVESTIMENTOS   
-    [6] EXPORTAR RELATÓRIO                              
-    [7] SAIR  
-    
-    Insira o número da opção desejada: ''')
+        Insira o número da opção desejada: ''')
 
-    try:
+        try:
 
-        if operacao in aux.keys():
-            aux.get(operacao)
+            if operacao in aux.keys():
+                aux.get(operacao)
 
-        else:
-            tentativas += 1
-
-            if tentativas == 3:
-                print(
-                    "Você atingiu o número máximo de tentativas. Encerrando o programa.")
-                encerrar_programa()
             else:
-                print(
-                    f"Entrada inválida. Você tem mais {3 - tentativas} {'tentativa' if tentativas == 2 else 'tentativas'}.")
-                break
+                tentativas += 1
 
-    except Exception as e:
-        print(f"Ocorreu um erro: {e}")
+                if tentativas == 3:
+                    print(
+                        "Você atingiu o número máximo de tentativas. Encerrando o programa.")
+                    encerrar_programa()
+                else:
+                    print(
+                        f"Entrada inválida. Você tem mais {3 - tentativas} {'tentativa' if tentativas == 2 else 'tentativas'}.")
+                    break
+
+        except Exception as e:
+            print(f"Ocorreu um erro: {e}")
 
 
 def le_registro():
