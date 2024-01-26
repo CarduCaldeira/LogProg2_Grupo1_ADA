df2e90afc6142e3cdcbacb8e73d22fa17221b967
Becker-RS <rafael.becker.sentena@gmail.com>
2024-01-25 22:56:58 -0300

    Att main.py in branch Becker

--- source/main.py
+++ source/main.py
@@ -1,77 +1,100 @@
-import pyfiglet 
+import pyfiglet
+import os
+import time
 from cria_registro import cria_registro
 from le_registro import le_registro
 from deleta_registro import deleta_registro
 from atualiza_registro import atualiza_registro
 from mostra_features import mostra_features
-from exporta_relatorio import exporta_relatorio 
+from exporta_relatorio import exporta_relatorio
 from encerrar_programa import encerrar_programa
 
 
-def main():
-    
-    f = pyfiglet.Figlet(font='big')
-    print(f.renderText('Bem Vindo ao sistema da Ceu!'))
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def tracos():
     print(f'{"="*70} \n'
-        f'{"-"*70} \n'
-        f'{"="*70} \n')
+          f'{"-"*70} \n'
+          f'{"="*70} \n')
+
+
+def tela_inicial():
+    f = pyfiglet.Figlet(font='big')
+    print(f.renderText('Bem Vindo ao Sistema da Ceu!'))
+    tracos()
+
+
+def main():
+
+    tela_inicial()
+
+    menu = {
+        '1': cria_registro,
+        '2': le_registro,
+        '3': atualiza_registro,
+        '4': deleta_registro,
+        '5': mostra_features,
+        '6': exporta_relatorio,
+        '7': encerrar_programa
+    }
 
     tentativas = 0
 
     while True:
-        operacao = input('''Digite a opção que deseja realizar:
-                            
-                        [1] CRIAR UM REGISTRO                             
-                        [2] LER UM REGISTRO                               
-                        [3] ATUALIZAR UM REGISTRO                         
-                        [4] DELETAR UM REGISTRO                           
-                        [5] INFORMAÇÕES SOBRE SEUS INVESTIMETOS   
-                        [6] EXPORTAR RELATÓRIO                              
-                        [7] SAIR  
-                        
-                         Insira o número da opção desejada: ''')                                        
-
-
-        if operacao == '1':
-            
-            cria_registro()
+        operacao = input('''                         
+    DIGITE UMA DAS OPÇÕES ABAIXO:
+    
+    [1] CRIAR UM REGISTRO
+    [2] LER UM REGISTRO
+    [3] ATUALIZAR UM REGISTRO
+    [4] DELETAR UM REGISTRO
+    [5] INFORMAÇÕES SOBRE SEUS INVESTIMENTOS
+    [6] EXPORTAR RELATÓRIO
+    [7] SAIR
         
-        elif operacao == '2':
-            
-            le_registro()
+     ''')
 
-        elif operacao == '3':
+        try:
+            if operacao in menu:
+                limpar_tela()
+                tracos()
+                menu[operacao]()
+                break
 
-            deleta_registro()
+            else:
+                tentativas += 1
 
-        elif operacao == '4':
-            
-            atualiza_registro()
-     
-        elif operacao == '5':
-            
-            mostra_features()
+                if tentativas == 3:
+                    print("Você atingiu o número máximo de tentativas.")
+                    encerrar_programa()
+                else:
+                    limpar_tela()
+                    print(
+                        f"'{operacao}' Não é uma opção válida. Você tem mais {3 - tentativas} {'tentativa' if tentativas == 2 else 'tentativas'}.")
+                    time.sleep(2)
+                    limpar_tela()
+                    tela_inicial()
+        except Exception as e:
+            print(f"Ocorreu um erro: {e}")
 
-        elif operacao == '6':
-            
-            exporta_relatorio()
-     
-        elif operacao == '7':
-            
-            encerrar_programa()
-            break
 
 def le_registro():
     pass
 
+
 def atualiza_registro():
     pass
+
 
 def deleta_registro():
     pass
 
+
 def mostra_features():
     pass
+
 
 def exporta_relatorio():
     pass
@@ -79,5 +102,3 @@ def exporta_relatorio():
 
 if __name__ == "__main__":
     main()
-    # %%
-    
