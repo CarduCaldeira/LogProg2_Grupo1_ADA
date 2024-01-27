7635bf796d2af5bda69eb360bc6188ae80ab4f41
Guilherme Oliveira <silva.guilhermeoliveira@gmail.com>
2024-01-26 21:45:41 -0300

    Ok

--- source/atualiza_registro.py
+++ source/atualiza_registro.py
@@ -2,21 +2,21 @@ import datetime
 import csv
 from utils import valida_data, determina_data
 
+
 def le_arquivo():
     pass
 
 
 def mostrar_opcoes(acao_realizada):
 
-    
-    registros_path = {  '1': 'registros_receita.csv',
-                        '2': 'registros_despesa.csv',
-                        '3': 'registros_investimento.csv'
-                    }
-    
+    registros_path = {'1': 'registros_receita.csv',
+                      '2': 'registros_despesa.csv',
+                      '3': 'registros_investimento.csv'
+                      }
+
     while True:
 
-        #fazer validao 
+        # fazer validao
         operacao = input('''Informe o tipo de operação do registro que deseja {acao_realizada} ou se 
                             deseja cancelar: 
                         
@@ -24,64 +24,63 @@ def mostrar_opcoes(acao_realizada):
                         [2] DESPESA
                         [3] INVESTIMENTO
                         [4] CANCELAR''')
-        
-        if operacao =='4':
+
+        if operacao == '4':
 
             return None
-        
+
         elif operacao in registros_path:
-           
+
             lista_registro = le_arquivo(registros_path[operacao])
 
             if not lista_registro:
 
                 print("Não há registros com esse tipo de operação! ")
                 return None
-            
+
             data = []
             print("Informe um intervalo de data em que a operação desejada pertence \n")
             for i in ["antiga", "recente"]:
-                print("Para a data mais antiga informe \n") #O dia,mes ano
+                print("Para a data mais antiga informe \n")  # O dia,mes ano
                 for tempo in ['dia', 'mes', 'ano']:
 
                     data.append(valida_data(tempo))
-                    #imaginar que em um app o usuario escolheria um intervalo 
-                    #de data em um calendario
+                    # imaginar que em um app o usuario escolheria um intervalo
+                    # de data em um calendario
 
             data = tuple(data)
-            #seleciona os registros ques estao neste intevalo
-            #aqui da pra usar funçao lambda e o filter
-            #depois de selecionado printa os 5 primeiros registros dessa lista (para nao ficar muito grande 
-            #a mensagem na tela), o usuario retorna entao o id 
+            # seleciona os registros ques estao neste intevalo
+            # aqui da pra usar funçao lambda e o filter
+            # depois de selecionado printa os 5 primeiros registros dessa lista (para nao ficar muito grande
+            # a mensagem na tela), o usuario retorna entao o id
         else:
             print("Opção inválida, informe a opção novamente!")
             continue
-           
-        
-        #funcao_q_verifca quantas opcoes de registros com tal operação
 
-          
+        # funcao_q_verifca quantas opcoes de registros com tal operação
+
 
 def get_id(acao_realizada):
     """
     Função que retorna o id a ser atualizado/deletado
     espeficicando o tipo de operacaçao, a data, e a despesa
-    """                                   
+    """
 
     id = mostrar_opcoes(acao_realizada)
 
     return id
 
 
-def atualiza_registros():
-  
+def atualiza_registro():
+
     id = get_id()
-    
+
     # Obter a nova data
     data = determina_data()
 
     # Obter o novo registro
-    tipo = input("Digite o novo tipo de registro (despesa, receita, investimento): ")
+    tipo = input(
+        "Digite o novo tipo de registro (despesa, receita, investimento): ")
 
     # Obter o novo valor do registro
     valor = float(input("Digite o novo valor do registro: "))
@@ -98,7 +97,6 @@ def atualiza_registros():
     with open("registros/registros_{operacao}.csv", "w") as arquivo:
         # Ler as linhas do arquivo
         linhas = arquivo.writer()
-
 
     # Atualizar o registro
     linhas[id] = f"{data},{tipo},{valor}\n"
