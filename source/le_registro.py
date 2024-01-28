e5c8d18eb59f587e833c0b36fa19a0bd486f9935
Becker-RS <rafael.becker.sentena@gmail.com>
2024-01-27 23:52:30 -0300

    Le registro

--- source/le_registro.py
+++ source/le_registro.py
@@ -1,9 +1,48 @@
 def le_registro():
-  # Abrir o arquivo
-  with open("registros.csv", "r") as arquivo:
-    # Ler as linhas do arquivo
-    linhas = arquivo.readlines()
 
-    # Imprimir as linhas
-    for linha in linhas:
-      print(linha)
+  repeat_question = True
+
+  while repeat_question:
+    menu_leitura = input('''Menu de leitura de registros:
+                       
+                       [1] DESPESA
+                       [2] RECEITA
+                       [3] INVESTIMENTOS
+                       [4] CANCELAR
+                       
+                          Digite o numero da opção de deseja acessar: ''')
+  
+    if menu_leitura not in ['1','2','3','4']:
+        print("Entrada invalida")
+
+    elif menu_leitura == '1':
+        leitura_despesa()
+
+    elif menu_leitura == '2':
+        leitura_receita()
+
+    elif menu_leitura == '3':
+        leitura_investimentos()
+
+
+
+
+def leitura_despesa():
+    with open("../registros/registros_despesa.csv", "r") as arquivo:
+        linhas = arquivo.readlines()
+
+        for linha in linhas:
+            print(linha)
+
+def leitura_receita():
+   with open("../registros/registros_receita.csv", "r") as arquivo:
+      linhas = arquivo.readlines()
+
+      for linha in linhas:
+            print(linha)
+
+def leitura_investimentos():
+   with open("../registros/investimento.csv", "r") as arquivo:
+      
+        for linha in linhas:
+           print(linha)
