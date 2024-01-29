c31bc488452d8bfc5a7676f31de8e716444c24b0
TacianaFer <taci.frnnds@gmail.com>
2024-01-29 19:27:08 -0300

    ajustada

--- source/agrupar_registro.py
+++ source/agrupar_registro.py
@@ -20,34 +20,4 @@ def agrupar_registro_por_tipo(registro_lista):
     
     return agrupamento
 
-    # Restante do código
-
-    while tentativas < max_tentativas:
-        limpar_tela()
-        tracos()
-        operacao = tela_inicial()
-
-        if operacao in operacoes:
-            if operacao == 1:
-                cria_registro(registro_lista)
-            elif operacao == 2:
-                le_registro(registro_lista)
-            elif operacao == 3:
-                atualizar_registro(registro_lista)
-            elif operacao == 4:
-                deletar_registro(registro_lista)
-            elif operacao == 5:
-                mostra_features(registro_lista)
-            elif operacao == 6:
-                exporta_relatorio(registro_lista)
-            elif operacao == 7:
-                encerrar_programa(registro_lista)
-        else:
-            tentativas += 1
-            print(f"{operacao} não é uma opção válida. "
-                  f"Você tem mais {max_tentativas - tentativas} tentativa{'s' if tentativas != max_tentativas - 1 else ''}.")
-            time.sleep(10)
-
-    print("Você atingiu o número máximo de tentativas. Encerrando o programa.")
-    time.sleep(2)
-    encerrar_programa(registro_lista)
+   
